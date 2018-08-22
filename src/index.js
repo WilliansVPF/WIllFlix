24abba4571ea4f654e8329b3cf1f2640dda9e679
Willians <williansvpf@gmail.com>
2018-08-22 17:30:01 -0300

    add react to the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import { request, save } from "./request"
+import { render } from "react-dom"
+import React from "react"
+import axios from "axios"
 
-request()
-save()
+class App extends React.PureComponent {
+    constructor(props){
+        super(props)
+        this.state = {movies:[], error:false}
+    }
+    
+    componentDidMount() {
+        axios.get("https://express-server2.herokuapp.com/movie").then( (response) => {            
+            this.setState({movies:response.data})
+        }).catch( () => {
+            this.setStnull({error:true})
+        })
+    }    
+    
+    render() {
+        const moviesHtml = this.state.movies.map((movie) => {
+            return <img src={movie.img} width="100" />
+        })
+        return moviesHtml
+    }
+}
+render(<App />, document.getElementById("App"))
